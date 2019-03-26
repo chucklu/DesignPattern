84333460479ef717eb16c79d4e68ac9121756fd1
Chuck Lu <chuck.lu@qq.com>
2019-03-26 20:01:50 +0800

    add Facade pattern

---
+++ 02Structural/Facade/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mortgage mortgage = new Mortgage();
+
+            Customer customer = new Customer("Ann McKinsey");
+            bool eligible = mortgage.IsEligible(customer, 125000);
+            Console.WriteLine("\n" + customer.Name + " has been " + (eligible ? "Approved" : "Rejected"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02Structural/Facade/SubSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Facade
+{
+    class Bank
+    {
+        public bool HasSufficientSavings(Customer c, int amount)
+        {
+            Console.WriteLine("Check bank for " + c.Name);
+            return true;
+        }
+    }
+
+    class Credit
+    {
+        public bool HasGoodCredit(Customer c)
+        {
+            Console.WriteLine("Check credit for " + c.Name);
+            return true;
+        }
+    }
+
+    class Loan
+    {
+        public bool HasNoBadLoans(Customer c)
+        {
+            Console.WriteLine("Check loans for " + c.Name);
+            return true;
+        }
+    }
+}
